2ebda8413d52f4adff91fd3e5ead8d14717367cb
Felix Hamann <felix@hamann.xyz>
2018-08-28 17:21:45 +0200

    setup project skeleton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Ungol WMD Distance',
+    version='0.1',
+    packages=['wmd', ],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
---
+++ wmd/dist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+
+
+import argparse
+
+
+def main():
+    pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
