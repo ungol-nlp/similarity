2ec6242ed35f47f7998c49d520151e1a45a44104
Felix Hamann <felix@hamann.xyz>
2018-08-31 15:16:47 +0200

    updated module structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Ungol WMD Distance',
     version='0.1',
-    packages=['ungol', ],
+    packages=['ungol.wmd', ],
     license='MIT',
     long_description=open('README.md').read(),
 )
