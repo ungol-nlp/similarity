eaba262550f809011ec9380531c339f4bc9d6dc0
Felix Hamann <felix@hamann.xyz>
2018-09-02 19:10:22 +0200

    refactored benchmarking; added benchmark for wmd.distance_matrix_*

--- ungol/wmd/benchmark.py
+++ ungol/wmd/benchmark.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from tabulate import tabulate
+
 import timeit
 import argparse
+from datetime import datetime
+
+from typing import List
+from typing import Tuple
+from typing import Callable
 
 
 # Use the min() rather than the average of the timings. That is a
@@ -15,7 +23,60 @@ import argparse
 # different implementations. – Raymond Hettinger Apr 3 '12
 
 
-setup = '''
+# timeit options
+REPS = (5, 1000)
+
+# ---
+
+
+def ts(info: str) -> Callable[[None], None]:
+    tstart = datetime.now()
+    print('running: {} - '.format(info), end='')
+
+    def _done(*args, **kwargs):
+        tdelta = (datetime.now() - tstart).total_seconds()
+        print('took: {delta}ms'.format(*args, delta=tdelta, **kwargs))
+
+    return _done
+
+
+def _timeit(cmd: str, setup: str):
+    return min(timeit.Timer(cmd, setup=setup).repeat(*REPS))
+
+
+def _run(info: str, cmd: str, setup: str):
+    done = ts(info)
+    res = _timeit(cmd, setup)
+    done()
+
+    return info, res
+
+
+def _benchmark(title: str, tasks: List[Tuple[str]]):
+
+    fmt = 'running {} benchmark ({} times, {} iterations)'
+    print(fmt.format(title, *REPS))
+    tab_data = []
+
+    for args in tasks:
+        tab_data.append(_run(*args))
+
+    tab_data.sort(key=lambda t: t[1])
+    print('\n', tabulate(tab_data, headers=('name', 'time')), '\n')
+
+
+def _debug_setup(setup: str):
+    eval(setup)
+    import pdb
+    pdb.set_trace()
+
+
+# ---
+
+
+def hamming_distance():
+
+    setup = '''
 from ungol.wmd import wmd
 import numpy as np
 
@@ -24,35 +85,112 @@ code1 = (np.random.randn(bytecount) * 255).astype(dtype=np.uint8)
 code2 = (np.random.randn(bytecount) * 255).astype(dtype=np.uint8)
 '''
 
+    _benchmark('hamming distance', [
+
+        ('binary counting with bin()',
+         'wmd.hamming_bincount(code1, code2)',
+         setup, ),
+
+        ('bitshifts with vanilla python',
+         'wmd.hamming_bitmask(code1, code2)',
+         setup, )
+
+    ])
+
+
+# ---
+
+
+def distance_matrix():
+
+    init = '''
+from ungol.wmd import wmd
+import numpy as np
+
+byte_c = 32
+n_max = 10000
+
+tokens = ['nope' for _ in range(n_max)]
+codes = (np.random.randn(n_max, byte_c) * 255).astype(dtype=np.uint8)
+dists = (np.random.randn(n_max, 4) * 255).astype(dtype=np.uint8)
+'''
+
+    benchmarks = []
+    for n in 10, 20, 30, 40, 50:
+        doc_init = '''
+doc = wmd.Doc(
+    tokens=tokens[:{n}],
+    codes=codes[:{n}],
+    dists=dists[:{n}], )
+'''.format(n=n)
+
+        setup = init + doc_init
+        benchmarks.append(
+
+            ('naive loop ({n} x {n})'.format(n=n),
+             'wmd.distance_matrix_loop(doc, doc)',
+             setup, ),
+
+            ('vectorized numpy ({n} x {n})'.format(n=n),
+             'wmd.distance_matrix_vectorized(doc, doc)',
+             setup, ),
+
+        )
+
+    _benchmark('distance matrix', benchmarks)
+
 
 def main():
     print('\n', 'welcome to vngol wmd benchmarks'.upper(), '\n')
 
-    repetitions = (7, 10000)
+    global REPS
+    mapping = (
+        'hamming distance',
+        'distance matrix', )
 
-    t_bincount = min(timeit.Timer(
-        'wmd.hamming_bincount(code1, code2)', setup=setup
-    ).repeat(*repetitions))
-    print('bincount time', t_bincount)
+    benchmarks = {'--' + key.replace(' ', '-'): key.replace(' ', '_')
+                  for key in mapping}
 
-    t_bitmask = min(timeit.Timer(
-        'wmd.hamming_bitmask(code1, code2)', setup=setup
-    ).repeat(*repetitions))
-    print('bitmask time:', t_bitmask)
-
-
-def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        '--all', action='store_true',
-        help='run all tests')
+    for option in benchmarks:
+        parser.add_argument(option, action='store_true')
+
+    # ---
 
     parser.add_argument(
-        '--hamming-distance', action='store_true',
-        help='run tests for hamming distance calculations')
+        '--run-all', action='store_true',
+        help='run all tests', )
 
-    return parser.parse_args()
+    parser.add_argument(
+        '--repeat', nargs=1, type=int, default=[REPS[0]],
+        help='how many times to call timeit', )
+
+    parser.add_argument(
+        '--number', nargs=1, type=int, default=[REPS[1]],
+        help='number argument for timeit()'
+    )
+
+    # ---
+
+    args = parser.parse_args()
+    dict_args = vars(args)
+
+    REPS = args.repeat[0], args.number[0]
+
+    if args.run_all:
+        print('running all benchmarks')
+        for benchmark in benchmarks.values():
+            globals()[benchmark]()
+
+    else:
+        if not any([dict_args[k] for k in benchmarks.values()]):
+            print('no benchmark selected...')
+            return
+
+        for benchmark in benchmarks.values():
+            if dict_args[benchmark]:
+                globals()[benchmark]()
 
 
 if __name__ == '__main__':
