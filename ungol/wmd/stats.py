684b3f397fa18f5d185a32e63420ef9ddcbdd55c
Felix Hamann <felix@hamann.xyz>
2018-09-23 15:15:20 +0200

    unified global/local cols/rows; added ScoreData.global_cols

--- ungol/wmd/stats.py
+++ ungol/wmd/stats.py
@@ -27,11 +27,12 @@ class ScoreData:
 
     docs: Tuple[wmd.Doc] = attr.ib()
 
-    common_unknown: Set[str] = attr.ib()
+    common_unknown: Set[str] = attr.ib(default=attr.Factory(set))
 
     # ---
 
     global_rows = attr.ib(default=attr.Factory(list))
+    global_columns = attr.ib(default=attr.Factory(list))
 
     local_rows = attr.ib(default=attr.Factory(list))
     local_columns = attr.ib(default=attr.Factory(list))
@@ -40,11 +41,6 @@ class ScoreData:
 
     def __attr_post_init__(self):
         assert(len(self.tokens))
-
-    def _unpack(self, ds: List[Tuple[str, Tuple[Any, Any]]]):
-        headers, columns = zip(*ds)
-        row1, row2 = zip(*columns)
-        return headers, row1, row2
 
     def _draw_border(self, s: str) -> str:
         lines = s.splitlines()
@@ -63,12 +59,14 @@ class ScoreData:
 
     # ---
 
-    def _str_global_table(self, buf: List[str]) -> None:
-        buf.append(f'\n{self.name} score : {self.score}\n')
+    def _str_rows(self, src, buf):
+        buf.append(tabulate(src))
+        buf.append('')
 
+    def _str_global_rows(self, buf: List[str]) -> None:
+        buf.append(f'\n{self.name} score : {self.score}\n')
         if len(self.global_rows):
-            buf.append(tabulate(self.global_rows))
-            buf.append('')
+            self._str_rows(self.global_rows, buf)
 
     def _str_local_rows(self, buf: List[str], a: int, b: int) -> None:
         name1, name2 = self.docs[a].name, self.docs[b].name
@@ -76,23 +74,42 @@ class ScoreData:
 
         if len(self.local_rows):
             rows = ((n, t[a]) for n, t in self.local_rows)
-            buf.append(tabulate(rows))
-            buf.append('')
+            self._str_rows(rows, buf)
 
         buf.append('')
 
-    def _str_local_columns(self, buf: List[str], a: int) -> None:
-        headers, *rows = self._unpack(self.local_columns)
-        rows = list(zip(*rows[a]))
+    # ---
 
+    def _str_cols(self, headers, rows, buf):
         headers = ('no', ) + headers
         rows.sort(key=lambda t: t[-1], reverse=True)
 
-        sims_table = tabulate(
+        table = tabulate(
             rows, headers=headers,
             showindex='always', floatfmt=".4f")
 
-        buf.append(sims_table)
+        buf.append(table)
+
+    def _str_local_columns(self, buf: List[str], a: int) -> None:
+        if not len(self.local_columns):
+            return
+
+        headers, columns = zip(*self.local_columns)
+
+        # extract the a-th data column
+        # note: a = zip(*zip(*a))
+        rows = list(zip(*list(zip(*columns))[a]))
+        self._str_cols(headers, rows, buf)
+
+    def _str_global_columns(self, buf: List[str]) -> None:
+        if not len(self.global_columns):
+            return
+
+        headers, columns = zip(*self.global_columns)
+        rows = list(zip(*columns))
+        self._str_cols(headers, rows, buf)
+
+    # ---
 
     def _str_common_unknown(self, buf: List[str], a: int) -> None:
         buf.append('\n')
@@ -113,7 +130,12 @@ class ScoreData:
     def __str__(self) -> str:
         buf = []
 
-        self._str_global_table(buf)
+        self._str_global_rows(buf)
+        buf.append('')
+
+        self._str_global_columns(buf)
+        buf.append('')
+
         buf.append(self.docstr(first=True) + '\n')
         buf.append(self.docstr(first=False) + '\n')
 
@@ -124,6 +146,9 @@ class ScoreData:
     def docstr(self, first: bool = True) -> str:
         a, b = (0, 1) if first else (1, 0)
 
+        if not len(self.local_rows) and not len(self.local_columns):
+            return f'No data for {self.docs[a].name}'
+
         buf = []
 
         self._str_local_rows(buf, a, b)
@@ -133,19 +158,30 @@ class ScoreData:
 
         return self._draw_border('\n'.join(buf))
 
-    def add_local_column(
-            self, name: str,
-            d1_data: np.array, d2_data:
-            np.array) -> None:
+    # ---
 
-        assert len(d1_data.shape) == 1 and len(d2_data.shape) == 1
-        assert d1_data.shape[0] == len(self.docs[0])
-        assert d2_data.shape[0] == len(self.docs[1])
+    def _add_column(self, coll, name, data):
+        if len(coll):
+            len_data, len_coll = len(data), len(coll[0][1])
+            assert len_data == len_coll, f'{len_data} =/= {len_coll} ({name})'
 
-        self.local_columns.append((name, (d1_data, d2_data)))
+        coll.append((name, data))
+
+    def add_local_column(self, name: str, d1: np.array, d2: np.array):
+        self._add_column(self.local_columns, name, (d1, d2))
+
+    def add_global_column(self, name: str, data: np.array):
+        self._add_column(self.global_columns, name, data)
+
+    def _add_row(self, coll, name, data):
+        if len(coll):
+            assert len(data[0]) == len(coll[0][1][0])
+            assert len(data[1]) == len(coll[0][1][1])
+
+        coll.append((name, data))
 
     def add_local_row(self, name: str, d1_data: Any, d2_data: Any):
-        self.local_rows.append((name, (d1_data, d2_data)))
+        self._add_row(self.local_rows, name, (d1_data, d2_data))
 
     def add_global_row(self, name: str, data: Any):
-        self.global_rows.append((name, data))
+        self._add_row(self.global_rows, name, data)
